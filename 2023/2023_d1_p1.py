fa53572f65859b3baa7a47e59948425799df7d2a
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2024-08-07 21:16:52 +0200

    Update 2023_d1_p1.py

--- 2023/2023_d1_p1.py
+++ 2023/2023_d1_p1.py
@@ -1,8 +1,6 @@
-
 sum = 0
 
 for line in open('input.txt', 'r'):
-    digits = [char for char in line if char.isdigit()]
-    sum += int(digits[0] + digits[-1])
-
+  digits = [char for char in line if char.isdigit()] # extract individual digits on each line
+  sum += int(digits[0] + digits[-1]) # create double digit number consisting of first and last digits observed in line, add to sum
 print(sum)
